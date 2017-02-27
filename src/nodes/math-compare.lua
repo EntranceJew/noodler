8f410b9fc001ecaf3524cc303a28bb8aa7d6cd6b
EntranceJew <EntranceJew@gmail.com>
2017-02-27 16:38:20 -0500

    Fixed view-boolean and math-compare, added inequality to math-compare.

--- src/nodes/math-compare.lua
+++ src/nodes/math-compare.lua
@@ -1,60 +1,43 @@
-local file = ...
 local node = require "node"
-
-local function mk_node(x, y)
-	return node {
-		file    = file,
-		name    = "Compare",
-		x       = x,
-		y       = y,
-		inputs  = {
-			{
-				label = "Number 1",
-				type  = "number"
-			}, {
-				label = "Number 2",
-				type  = "number"
-			},
-		},
-		outputs = {
-			{
-				label = "==",
-				type  = "boolean"
-			},
-			{
-				label = ">",
-				type  = "boolean"
-			},
-			{
-				label = ">=",
-				type  = "boolean"
-			},
-			{
-				label = "<",
-				type  = "boolean"
-			},
-			{
-				label = "<=",
-				type  = "boolean"
-			}
-		},
-		values = {
-			0,
-			0
-		},
-		evaluate = function(self)
-			return
-				self.values[1] == self.values[2],
-				self.values[1] >  self.values[2],
-				self.values[1] >= self.values[2],
-				self.values[1] <  self.values[2],
-				self.values[1] <= self.values[2]
-		end
-	}
-end
+local plug = require "plug"
 
 return {
 	name     = "Compare",
 	category = "Math",
-	new      = mk_node
+	new      = function(tree, position)
+		return tree:add(
+			node {
+				name = "Compare",
+				inputs  = {
+					plug("Number 1", "number", 1),
+					plug("Number 2", "number", 2)
+				},
+				outputs = {
+					plug("==", "boolean", 1),
+					plug("~=", "boolean", 2),
+					plug(">", "boolean", 3),
+					plug(">=", "boolean", 4),
+					plug("<", "boolean", 5),
+					plug("<=", "boolean", 6)
+				},
+				values = {
+					false,
+					false,
+					false,
+					false,
+					false,
+					false
+				},
+				evaluate = function(self)
+					self.computed[1] = self.values[1] == self.values[2]
+					self.computed[2] = self.values[1] ~= self.values[2]
+					self.computed[3] = self.values[1] >  self.values[2]
+					self.computed[4] = self.values[1] >= self.values[2]
+					self.computed[5] = self.values[1] <  self.values[2]
+					self.computed[6] = self.values[1] <= self.values[2]
+				end
+			},
+			position
+		)
+	end
 }
--- src/nodes/view-boolean.lua
+++ src/nodes/view-boolean.lua
@@ -1,35 +1,24 @@
-local file = ...
 local node = require "node"
-local cpml = require "cpml"
-
-local function mk_node(x, y)
-	return node {
-		file    = file,
-		name    = "Boolean View",
-		x       = x,
-		y       = y,
-		inputs  = {
-			{
-				label = "Boolean",
-				type  = "boolean"
-			}
-		},
-		values = {
-			false
-		},
-		evaluate = function(self) end,
-		display = function(self)
-			love.graphics.setColor(255, 255, 255, 255)
-			love.graphics.rectangle("fill", 0, 0, self.size.x, self.size.y)
-			love.graphics.setColor(0, 0, 0, 255)
-			love.graphics.print(tostring(self.values[1]), 5)
-		end,
-		size = cpml.vec2(100, 20)
-	}
-end
+local plug = require "plug"
 
 return {
 	name     = "Boolean",
 	category = "View",
-	new      = mk_node
+	new      = function(tree, position)
+		return tree:add(
+			node {
+				name = "Boolean View",
+				inputs = {
+					plug("Value", "boolean", 1)
+				},
+				values = {
+					false
+				},
+				display = function(self, ui)
+					ui:label(string.format("%s", tostring(self.values[1])))
+				end
+			},
+			position
+		)
+	end
 }
